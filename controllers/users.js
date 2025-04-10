8aed0243c3db55cc80f98e2ea6f253c704b61e67
Shivam lodhi <xx>
2025-04-10 18:50:33 +0530

    add Project File

--- controllers/users.js
+++ controllers/users.js
@@ -5,8 +5,8 @@ module.exports.rendersignupForm = (req, res)=>{
 module.exports.signup=async (req, res)=>{
    try{
       let {username, email, password}= req.body;
-   const newUser= new User({email, username});
-    const registerUser =await User.register(newUser, password);
+      const newUser= new User({email, username});
+      const registerUser =await User.register(newUser, password);
     
    console.log(registerUser);
    req.login(registerUser, (err)=>{
